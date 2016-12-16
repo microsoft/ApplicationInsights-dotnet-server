40044ae9eb7ab60798189d98b4b05809387e0851
Alexander Kaplan <alkaplan@microsoft.com>
2016-12-16 10:50:08 -0800

    Additional comments.

--- Src/PerformanceCollector/Shared/Implementation/QuickPulse/Process/QuickPulseProcessProvider.cs
+++ Src/PerformanceCollector/Shared/Implementation/QuickPulse/Process/QuickPulseProcessProvider.cs
@@ -28,6 +28,12 @@
             this.perfLib.Close();
         }
 
+        /// <summary>
+        /// Gets a collection of <see cref="QuickPulseProcess"/> objects - each corresponding to a system process and containing
+        /// information about the amount of time the process has occupied CPU cores.
+        /// </summary>
+        /// <param name="totalTime">If available, contains the value of the _Total instance of the counter, which indicates the overall
+        /// amount of time spent by CPU cores executing system processes.</param>
         public IEnumerable<QuickPulseProcess> GetProcesses(out TimeSpan? totalTime)
         {
             const int CategoryIndex = 230;
