fd1ea5442c8cad51111e6e56980eb21f14518dc1
Nizar Qamar <nizarq@microsoft.com>
2016-12-19 14:08:50 -0800

    Adding an extra null check to safeguard against a possible null reference.

--- Src/Web/Web.Shared.Net/RequestTrackingTelemetryModule.cs
+++ Src/Web/Web.Shared.Net/RequestTrackingTelemetryModule.cs
@@ -114,7 +114,9 @@
                 string sourceIkey = context.Request.Headers[RequestResponseHeaders.SourceInstrumentationKeyHeader];
 
                 // If the source header is present on the incoming request, and it is an external component (not the same ikey as the one used by the current component), populate the source field.
-                if (!string.IsNullOrEmpty(sourceIkey) && sourceIkey != InstrumentationKeyHashLookupHelper.GetInstrumentationKeyHash(requestTelemetry.Context.InstrumentationKey))
+                if (!string.IsNullOrEmpty(sourceIkey)
+                    && !string.IsNullOrEmpty(requestTelemetry.Context.InstrumentationKey)
+                    && sourceIkey != InstrumentationKeyHashLookupHelper.GetInstrumentationKeyHash(requestTelemetry.Context.InstrumentationKey))
                 {
                     requestTelemetry.Source = sourceIkey;
                 }
