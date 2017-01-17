663dc6386bc30af745a22c75245fc183e97ab64d
Alexander Kaplan <alkaplan@microsoft.com>
2017-01-17 14:22:37 -0800

    PR feedback.

--- Src/PerformanceCollector/Shared/Implementation/QuickPulse/Process/IQuickPulseProcessProvider.cs
+++ Src/PerformanceCollector/Shared/Implementation/QuickPulse/Process/IQuickPulseProcessProvider.cs
@@ -12,6 +12,12 @@
 
         void Close();
 
+        /// <summary>
+        /// Gets a collection of <see cref="QuickPulseProcess"/> objects - each corresponding to a system process and containing
+        /// information about the amount of time the process has occupied CPU cores.
+        /// </summary>
+        /// <param name="totalTime">If available, contains the value of the _Total instance of the counter, which indicates the overall
+        /// amount of time spent by CPU cores executing system processes.</param>
         IEnumerable<QuickPulseProcess> GetProcesses(out TimeSpan? totalTime);
     }
 }
