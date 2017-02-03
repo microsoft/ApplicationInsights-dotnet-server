b3cd49fcf04b8547a81c98cf52e63679d2f22301
Dan Schulte <daschult@microsoft.com>
2017-02-03 11:39:08 -0800

    Got .NET Core DependencyCollector to build

--- Src/DependencyCollector/NetCore/DependencyTrackingDiagnosticListener.cs
+++ Src/DependencyCollector/NetCore/DependencyTrackingDiagnosticListener.cs
@@ -7,26 +7,13 @@ using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Reflection;
-using Microsoft.Diagnostics.Context;
-using Microsoft.Diagnostics.Correlation.Common;
-using Microsoft.Diagnostics.Correlation.Common.Instrumentation;
 
 namespace Microsoft.Diagnostics.Correlation.AspNetCore.Internal
 {
-    internal class HttpDiagnosticListenerObserver<TContext> : IObserver<KeyValuePair<string, object>> where TContext : ICorrelationContext<TContext>
+    internal class DependencyTrackingDiagnosticListener : IObserver<KeyValuePair<string, object>>
     {
-        private readonly List<IContextInjector<TContext, HttpRequestMessage>> contextInjectors;
-        private readonly IEndpointFilter endpointFilter;
-        private readonly IOutgoingRequestNotifier<TContext, HttpRequestMessage, HttpResponseMessage> requestNotifier;
-
-        public HttpDiagnosticListenerObserver(
-            IEnumerable<IContextInjector<TContext, HttpRequestMessage>> contextInjectors,
-            IEndpointFilter endpointFilter,
-            IOutgoingRequestNotifier<TContext, HttpRequestMessage, HttpResponseMessage> requestNotifier)
+        public DependencyTrackingDiagnosticListener()
         {
-            this.endpointFilter = endpointFilter;
-            this.contextInjectors = new List<IContextInjector<TContext, HttpRequestMessage>>(contextInjectors);
-            this.requestNotifier = requestNotifier;
         }
 
         public void OnNext(KeyValuePair<string, object> value)
@@ -36,32 +23,19 @@ namespace Microsoft.Diagnostics.Correlation.AspNetCore.Internal
 
             if (value.Key == "System.Net.Http.Request")
             {
-                PropertyInfo requestInfo = value.Value.GetType().GetProperty("Request");
+                PropertyInfo requestInfo = value.Value.GetType().GetRuntimeProperty("Request");
                 HttpRequestMessage request = (HttpRequestMessage)requestInfo?.GetValue(value.Value, null);
 
                 if (request != null)
                 {
-                    var ctx = ContextResolver.GetContext<TContext>();
-                    if (endpointFilter.Validate(request.RequestUri))
-                    {
-                        foreach (var injector in contextInjectors)
-                        {
-                            injector.UpdateRequest(ctx, request);
-                        }
-                        requestNotifier.OnBeforeRequest(ctx, request);
-                    }
                 }
             }
             else if (value.Key == "System.Net.Http.Response")
             {
-                var responseInfo = value.Value.GetType().GetProperty("Response");
-                var response = (HttpResponseMessage)responseInfo?.GetValue(value.Value, null);
+                PropertyInfo responseInfo = value.Value.GetType().GetRuntimeProperty("Response");
+                HttpResponseMessage response = (HttpResponseMessage)responseInfo?.GetValue(value.Value, null);
                 if (response != null)
                 {
-                    if (endpointFilter.Validate(response.RequestMessage.RequestUri))
-                    {
-                        requestNotifier.OnAfterResponse(ContextResolver.GetContext<TContext>(), response);
-                    }
                 }
             }
         }
