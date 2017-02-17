067a609023a33fb2fa08e120b898a54147bf6908
Cijo Thomas <cithomas@microsoft.com>
2017-02-17 09:18:57 -0800

    Port number added to Dependency Target field for non standard ports 80,443. Unit tests.

---
+++ Src/DependencyCollector/Shared/Implementation/DependencyTargetNameHelper.cs
@@ -0,0 +1,31 @@
+﻿namespace Microsoft.ApplicationInsights.DependencyCollector.Implementation
+{
+    using System;   
+
+    /// <summary>
+    /// Dependency TargetName helper.
+    /// </summary>
+    internal static class DependencyTargetNameHelper
+    {
+        private const int HttpPort = 80;
+        private const int HttpsPort = 443;
+
+        /// <summary>
+        /// Returns dependency target name from the given Uri.
+        /// Port name is included in target for non-standard ports.
+        /// </summary>
+        /// <param name="uri">Dependency url from which target is to be extracted.</param>        
+        /// <returns>Dependency target name.</returns>
+        internal static string GetDependencyTargetName(Uri uri)
+        {
+            if (uri.Port != HttpPort && uri.Port != HttpsPort)
+            {
+                return uri.Host + ":" + uri.Port;
+            }
+            else
+            {
+                return uri.Host;
+            }
+        }      
+    }
+}
