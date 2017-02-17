88c52c26c437a3a287dc0cb7f1d042a6aef6a20a
Cijo Thomas <cithomas@microsoft.com>
2017-02-17 09:33:33 -0800

    Minor refactor

--- Src/DependencyCollector/Shared/Implementation/DependencyTargetNameHelper.cs
+++ Src/DependencyCollector/Shared/Implementation/DependencyTargetNameHelper.cs
@@ -1,6 +1,7 @@
 ﻿namespace Microsoft.ApplicationInsights.DependencyCollector.Implementation
 {
-    using System;   
+    using System;
+    using System.Globalization;
 
     /// <summary>
     /// Dependency TargetName helper.
@@ -14,18 +15,18 @@
         /// Returns dependency target name from the given Uri.
         /// Port name is included in target for non-standard ports.
         /// </summary>
-        /// <param name="uri">Dependency url from which target is to be extracted.</param>        
+        /// <param name="uri">Dependency uri from which target is to be extracted.</param>        
         /// <returns>Dependency target name.</returns>
         internal static string GetDependencyTargetName(Uri uri)
         {
             if (uri.Port != HttpPort && uri.Port != HttpsPort)
             {
-                return uri.Host + ":" + uri.Port;
+                return string.Format(CultureInfo.InvariantCulture, "{0}:{1}", uri.Host, uri.Port);
             }
             else
             {
                 return uri.Host;
             }
         }      
-    }
+    } 
 }
