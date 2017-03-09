3dfcf0b8dacbc78e170b95bd5f02841198127cdc
Cijo Thomas <cithomas@microsoft.com>
2017-03-09 12:07:04 -0800

    added exe proj.

---
+++ Src/AIPerfAgent/PerformanceCounterData.cs
@@ -0,0 +1,41 @@
+﻿namespace AIPerfAgent
+{
+    public class PerformanceCounterData
+    {
+        public PerformanceCounterData(
+            string originalString,
+            string reportAs,
+            bool usesInstanceNamePlaceholder,
+            bool isCustomCounter,
+            bool isInBadState,
+            string categoryName,
+            string counterName,
+            string instanceName)
+        {
+            this.OriginalString = originalString;
+            this.ReportAs = reportAs;
+            this.UsesInstanceNamePlaceholder = usesInstanceNamePlaceholder;
+            this.IsCustomCounter = isCustomCounter;
+            this.IsInBadState = isInBadState;
+            this.CategoryName = categoryName;
+            this.CounterName = counterName;
+            this.InstanceName = instanceName;
+        }
+
+        public string OriginalString { get; private set; }
+
+        public string ReportAs { get; private set; }
+
+        public bool UsesInstanceNamePlaceholder { get; private set; }
+
+        public bool IsCustomCounter { get; private set; }
+
+        public bool IsInBadState { get; private set; }
+
+        public string CategoryName { get; private set; }
+
+        public string CounterName { get; private set; }
+
+        public string InstanceName { get; private set; }
+    }
+}
---
+++ Src/AIPerfAgent/Program.cs
@@ -0,0 +1,146 @@
+﻿using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.ApplicationInsights.Extensibility.PerfCounterCollector;
+using Microsoft.ApplicationInsights.Extensibility.PerfCounterCollector.QuickPulse;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AIPerfAgent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuickPulseViaNugetReference();
+            Console.ReadLine();
+        }
+
+        private static void QuickPulseViaNugetReference()
+        {
+            TelemetryConfiguration configuration = new TelemetryConfiguration();
+            configuration.InstrumentationKey = "9d3ebb4f-7a11-4fb1-91ac-7ca8a17627eb";
+
+            QuickPulseTelemetryProcessor processor = null;
+
+            configuration.TelemetryProcessorChainBuilder
+                .Use((next) =>
+                {
+                    processor = new QuickPulseTelemetryProcessor(next);
+                    return processor;
+                })
+                .Build();
+
+            var QuickPulse = new QuickPulseTelemetryModule();
+            QuickPulse.Initialize(configuration);
+            QuickPulse.RegisterTelemetryProcessor(processor);
+
+            Console.WriteLine("Started..");
+            Console.ReadLine();
+
+        }
+
+        private static void PerfCollectorViaNugetReference()
+        {
+            TelemetryConfiguration configuration = new TelemetryConfiguration();
+            configuration.InstrumentationKey = "9d3ebb4f-7a11-4fb1-91ac-7ca8a17627eb";
+
+            PerformanceCollectorModule perf = new PerformanceCollectorModule();
+            perf.Initialize(configuration);
+
+            Console.WriteLine("Started..");
+            Console.ReadLine();
+              
+
+        }
+
+        private static void PerfCollectorViaSourceCopy()
+        {
+            StandardPerformanceCollector collector = new StandardPerformanceCollector();
+            TelemetryClient client = new TelemetryClient();
+            client.InstrumentationKey = "2ef2a25f-73c6-4cf6-99bd-ba7cfe13d3c1";
+
+            List<PerformanceCounterCollectionRequest> defaultCounters = new List<PerformanceCounterCollectionRequest>();
+            defaultCounters.Add(new PerformanceCounterCollectionRequest(@"\Process(??APP_WIN32_PROC??)\% Processor Time", @"\Process(??APP_WIN32_PROC??)\% Processor Time"));
+            defaultCounters.Add(new PerformanceCounterCollectionRequest(@"\Memory\Available Bytes", @"\Memory\Available Bytes"));
+
+            string error;
+            var errors = new List<string>();
+            foreach (PerformanceCounterCollectionRequest req in defaultCounters)
+            {
+                collector.RegisterCounter(
+                        req.PerformanceCounter,
+                        req.ReportAs,
+                        true,
+                        out error,
+                        false);
+                if (!string.IsNullOrWhiteSpace(error))
+                {
+                    errors.Add(
+                        string.Format(
+                            CultureInfo.InvariantCulture,
+                            //Resources.PerformanceCounterCheckConfigurationEntry,
+                            "Fail",
+                            req.PerformanceCounter,
+                            error));
+                }
+            }
+
+            while (true)
+            {
+                var results =
+                       collector.Collect(
+                           (counterName, e) =>
+                           PerformanceCollectorEventSource.Log.CounterReadingFailedEvent(e.ToString(), counterName))
+                           .ToList();
+
+                foreach (var result in results)
+                {
+                    var telemetry = CreateTelemetry(result.Item1, result.Item2);
+                    try
+                    {
+                        client.Track(telemetry);
+                    }
+                    catch (InvalidOperationException e)
+                    {
+                        PerformanceCollectorEventSource.Log.TelemetrySendFailedEvent(e.ToString());
+                    }
+                }
+
+                Thread.Sleep(10000);
+            }
+        }
+
+        private static MetricTelemetry CreateTelemetry(PerformanceCounterData pc, double value)
+        {
+            var metricName = !string.IsNullOrWhiteSpace(pc.ReportAs)
+                                 ? pc.ReportAs
+                                 : string.Format(
+                                     CultureInfo.InvariantCulture,
+                                     "{0} - {1}",
+                                     pc.CategoryName,
+                                     pc.CounterName);
+
+            var metricTelemetry = new MetricTelemetry()
+            {
+                Name = metricName,
+                Count = 1,
+                Sum = value,
+                Min = value,
+                Max = value,
+                StandardDeviation = 0
+            };
+
+            metricTelemetry.Properties.Add("CounterInstanceName", pc.InstanceName);
+            metricTelemetry.Properties.Add("CustomPerfCounter", "true");
+
+            return metricTelemetry;
+        }
+
+    }
+}
