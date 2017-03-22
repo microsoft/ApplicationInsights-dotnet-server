a1ecce70a52dba727e8bc8e667336969672a9b3e
iusafaro <iusafaro@microsoft.com>
2017-03-22 12:42:55 -0700

    Remove unnecessary using

--- Src/Web/Web.Shared.Net.Tests/RequestTrackingTelemetryModuleTest.cs
+++ Src/Web/Web.Shared.Net.Tests/RequestTrackingTelemetryModuleTest.cs
@@ -17,7 +17,6 @@
     using Microsoft.VisualStudio.TestTools.UnitTesting;
 
     using Assert = Xunit.Assert;
-    using System.Reflection;
 
     [TestClass]
     public class RequestTrackingTelemetryModuleTest
