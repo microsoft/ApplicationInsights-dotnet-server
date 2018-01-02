9d274e7c8104779959961230f421b8c571c630d0
Timothy Mothra Lee <tilee@microsoft.com>
2018-01-02 10:08:39 -0800

    fixing problems identified by VS Code

--- SDL/BinSkimSetup.ps1
+++ SDL/BinSkimSetup.ps1
@@ -17,7 +17,7 @@ Write-Host "binSkimDir:" $binSkimDirectory;
 Function CreateTargetDirectory ($folder) {
     # don't need to clean folder on build server, but is needed for local dev
     if (Test-Path $folder) { Remove-Item $folder -Recurse; }
-    md $folder
+    mkdir $folder
 }
 
 
@@ -25,7 +25,7 @@ CreateTargetDirectory $binSkimDirectory
 
 Get-ChildItem -Path $buildDirectory -Recurse *.dll |
  Where-Object {$_.DirectoryName -notMatch "Test"} |
- %{
+ ForEach-Object {
     # copy files, maintaining subdirectories
     Write-Host " "
 
@@ -36,13 +36,13 @@ Get-ChildItem -Path $buildDirectory -Recurse *.dll |
     Write-Host "sourceDir:" $sourceDir
     Write-Host "destDir:" $destDir
 
-    if (!(Test-Path $destDir)) { md $destDir }
+    if (!(Test-Path $destDir)) { mkdir $destDir }
     if (!(Test-Path $destFile)) { Copy-Item -Path $_.FullName -Destination $destDir }
     #Copy-Item -Path $_.FullName -Destination $destDir
 } 
 
 
 # summary for log output
-$count = Get-ChildItem -Path $binSkimDirectory -Recurse -File | Measure-Object | %{$_.Count}
+$count = Get-ChildItem -Path $binSkimDirectory -Recurse -File | Measure-Object | ForEach-Object {$_.Count}
 Write-Host " "
 Write-Host "Total Files:" $count
