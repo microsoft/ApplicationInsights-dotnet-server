7ef1297c453e7bac1d42d532b12529197a30c4de
Timothy Mothra Lee <tilee@microsoft.com>
2018-01-29 15:37:04 -0800

    consolidate another duplicate test file. add pragma restore

--- Src/PerformanceCollector/Perf.Shared.Tests/QuickPulse/QuickPulseTestHelper.cs
+++ Src/PerformanceCollector/Perf.Shared.Tests/QuickPulse/QuickPulseTestHelper.cs
@@ -2,7 +2,11 @@
 {
     using System;
     using System.Collections.Generic;
+#if NETCORE
     using System.Reflection;
+#else
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+#endif
 
     using Microsoft.ApplicationInsights.Extensibility;
     using Microsoft.ApplicationInsights.Extensibility.Implementation;
@@ -11,7 +15,7 @@
     /// <summary>
     /// Part of the <see cref="QuickPulseTestHelper"/> class for all .NET Frameworks.
     /// </summary>
-    internal static partial class QuickPulseTestHelper
+    internal static class QuickPulseTestHelper
     {
         public static LinkedList<IQuickPulseTelemetryProcessor> GetTelemetryProcessors(QuickPulseTelemetryModule module)
         {
@@ -22,6 +26,30 @@
         {
             SetPrivateStaticField(typeof(TelemetryConfiguration), "active", null);
             TelemetryModules.Instance.Modules.Clear();
+        }
+
+        public static object GetPrivateField(object obj, string fieldName)
+        {
+#if NETCORE
+            TypeInfo type = obj.GetType().GetTypeInfo();
+            FieldInfo field = type.GetField(fieldName, BindingFlags.Instance | BindingFlags.NonPublic);
+            return field.GetValue(obj);
+#else
+            var t = new PrivateObject(obj);
+            return t.GetField(fieldName);
+#endif
+        }
+
+        private static void SetPrivateStaticField(Type type, string fieldName, object value)
+        {
+#if NETCORE
+            TypeInfo typeInfo = type.GetTypeInfo();
+            FieldInfo field = typeInfo.GetField(fieldName, BindingFlags.NonPublic | BindingFlags.Static);
+            field.SetValue(null, value);
+#else
+            PrivateType t = new PrivateType(type);
+            t.SetStaticField(fieldName, value);
+#endif
         }
     }
 }
