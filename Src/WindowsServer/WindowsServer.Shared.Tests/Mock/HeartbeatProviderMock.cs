08c3a57dc54bd438e8d079c0cc4130219715cd5c
Derek Keeler <dekeeler@microsoft.com>
2018-03-05 11:56:20 -0800

    Fix for build problem with base SDK 2.6.0-beta1
    - preparation for beta2

--- Src/WindowsServer/WindowsServer.Shared.Tests/Mock/HeartbeatProviderMock.cs
+++ Src/WindowsServer/WindowsServer.Shared.Tests/Mock/HeartbeatProviderMock.cs
@@ -9,14 +9,17 @@
         public bool Enabled = true;
         public TimeSpan Interval = TimeSpan.FromMinutes(15);
         public List<string> ExcludedProps = new List<string>();
+		public List<string> ExcludedPropProviders = new List<string>();
         public Dictionary<string, string> HbeatProps = new Dictionary<string, string>();
         public Dictionary<string, bool> HbeatHealth = new Dictionary<string, bool>();
-
+		
         public bool IsHeartbeatEnabled { get => this.Enabled; set => this.Enabled = value; }
 
         public TimeSpan HeartbeatInterval { get => this.Interval; set => this.Interval = value; }
 
         public IList<string> ExcludedHeartbeatProperties => this.ExcludedProps;
+		
+		public IList<string> ExcludedHeartbeatPropertyProviders => this.ExcludedPropProviders;
 
         public bool AddHeartbeatProperty(string propertyName, string propertyValue, bool isHealthy)
         {
