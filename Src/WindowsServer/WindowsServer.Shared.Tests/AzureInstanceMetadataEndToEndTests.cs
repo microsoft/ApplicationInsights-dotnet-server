04c78a3875851c9e848dec360664179cd5ab6482
Derek Keeler <dekeeler@microsoft.com>
2018-03-09 09:28:43 -0800

    Cleanup test methods for heartbeat end to end

--- Src/WindowsServer/WindowsServer.Shared.Tests/AzureInstanceMetadataEndToEndTests.cs
+++ Src/WindowsServer/WindowsServer.Shared.Tests/AzureInstanceMetadataEndToEndTests.cs
@@ -12,67 +12,38 @@ namespace Microsoft.ApplicationInsights.WindowsServer
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using Assert = Xunit.Assert;
 
+    /// <summary>
+    /// Tests the heartbeat functionality through actual local-only Http calls to mimic
+    /// end to end functionality as closely as possible.
+    /// </summary>
     [TestClass]
     public class AzureInstanceMetadataEndToEndTests
     {
-        private MemoryStream jsonStream;
-
-        public AzureInstanceMetadataEndToEndTests()
-        {
-            this.TestComputeMetadata = new AzureInstanceComputeMetadata()
-            {
-                Location = "US-West",
-                Name = "test-vm01",
-                Offer = "D9_USWest",
-                OsType = "Linux",
-                PlacementGroupId = "placement-grp",
-                PlatformFaultDomain = "0",
-                PlatformUpdateDomain = "0",
-                Publisher = "Microsoft",
-                ResourceGroupName = "test.resource-group_01",
-                Sku = "Windows_10",
-                SubscriptionId = Guid.NewGuid().ToString(),
-                Tags = "thisTag;thatTag",
-                Version = "10.8a",
-                VmId = Guid.NewGuid().ToString(),
-                VmSize = "A8"
-            };
-
-            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(AzureInstanceComputeMetadata));
-            this.jsonStream = new MemoryStream();
-            serializer.WriteObject(this.jsonStream, this.TestComputeMetadata);
-        }
-
-        private AzureInstanceComputeMetadata TestComputeMetadata { get; set; }
+        internal const string MockTestUri = "http://localhost:9922/";
 
         [TestMethod]
         public void SpoofedResponseFromAzureIMSDoesntCrash()
         {
             CancellationTokenSource cts = new CancellationTokenSource();
-
-            string mockUri = "http://localhost:9922/";
+            var testMetadata = this.GetTestMetadata();
 
             using (new AzureInstanceMetadataServiceMock(
-                mockUri, 
+                AzureInstanceMetadataEndToEndTests.MockTestUri, 
                 (HttpListenerContext context) =>
             {
                 HttpListenerResponse response = context.Response;
-
-                // Construct a response.
-                response.ContentEncoding = Encoding.UTF8;
-
-                // Get a response stream and write the response to it.
-                this.jsonStream.Position = 0;
-                response.ContentLength64 = (int)this.jsonStream.Length;
-                this.jsonStream.Position = 0;
-                context.Response.ContentType = "application/json";
-                this.jsonStream.WriteTo(context.Response.OutputStream);
                 context.Response.StatusCode = 200;
+
+                response.ContentEncoding = Encoding.UTF8;
+                var jsonStream = this.GetTestMetadataStream(testMetadata);
+                response.ContentLength64 = (int)jsonStream.Length;
+                context.Response.ContentType = "application/json";
+                jsonStream.WriteTo(context.Response.OutputStream);
             }))
             {
                 var azureIms = new AzureMetadataRequestor
                 {
-                    BaseAimsUri = mockUri
+                    BaseAimsUri = AzureInstanceMetadataEndToEndTests.MockTestUri
                 };
 
                 var azureImsProps = new AzureComputeMetadataHeartbeatPropertyProvider();
@@ -83,7 +54,7 @@ namespace Microsoft.ApplicationInsights.WindowsServer
                 {
                     string fieldValue = azureIMSData.Result.GetValueForField(fieldName);
                     Assert.NotNull(fieldValue);
-                    Assert.Equal(fieldValue, this.TestComputeMetadata.GetValueForField(fieldName));
+                    Assert.Equal(fieldValue, testMetadata.GetValueForField(fieldName));
                 }
             }
         }
@@ -93,35 +64,30 @@ namespace Microsoft.ApplicationInsights.WindowsServer
         {
             CancellationTokenSource cts = new CancellationTokenSource();
 
-            string mockUri = "http://localhost:9922/";
-
             using (new AzureInstanceMetadataServiceMock(
-                mockUri, 
+                AzureInstanceMetadataEndToEndTests.MockTestUri, 
                 (HttpListenerContext context) =>
             {
                 HttpListenerResponse response = context.Response;
+                context.Response.StatusCode = 200;
 
                 // Construct a response just like in the positive test but triple it.
                 response.ContentEncoding = Encoding.UTF8;
 
                 // Get a response stream and write the response to it.
-                this.jsonStream.Position = 0;
-                response.ContentLength64 = 3 * (int)this.jsonStream.Length;
-                this.jsonStream.Position = 0;
+                var jsonStream = this.GetTestMetadataStream();
+                response.ContentLength64 = 3 * (int)jsonStream.Length;
                 context.Response.ContentType = "application/json";
-
-                this.jsonStream.WriteTo(context.Response.OutputStream);
-                this.jsonStream.Position = 0;
-                this.jsonStream.WriteTo(context.Response.OutputStream);
-                this.jsonStream.Position = 0;
-                this.jsonStream.WriteTo(context.Response.OutputStream);
-
-                context.Response.StatusCode = 200;
+                jsonStream.WriteTo(context.Response.OutputStream);
+                jsonStream.Position = 0;
+                jsonStream.WriteTo(context.Response.OutputStream);
+                jsonStream.Position = 0;
+                jsonStream.WriteTo(context.Response.OutputStream);
             }))
             {
                 var azureIms = new AzureMetadataRequestor
                 {
-                    BaseAimsUri = mockUri
+                    BaseAimsUri = AzureInstanceMetadataEndToEndTests.MockTestUri
                 };
 
                 var azureIMSData = azureIms.GetAzureComputeMetadataAsync();
@@ -136,35 +102,29 @@ namespace Microsoft.ApplicationInsights.WindowsServer
         {
             CancellationTokenSource cts = new CancellationTokenSource();
 
-            string mockUri = "http://localhost:9922/";
-
             using (new AzureInstanceMetadataServiceMock(
-                mockUri, 
+                AzureInstanceMetadataEndToEndTests.MockTestUri, 
                 (HttpListenerContext context) =>
             {
                 HttpListenerResponse response = context.Response;
+                context.Response.StatusCode = 200;
 
-                var malformedData = this.TestComputeMetadata;
+                // make it a malicious-ish response...
+                var malformedData = this.GetTestMetadata();
                 malformedData.Name = "Not allowed for VM names";
                 malformedData.ResourceGroupName = "Not allowed for resource group name";
                 malformedData.SubscriptionId = "Definitely-not-a GUID up here";
-                var serializer = new DataContractJsonSerializer(typeof(AzureInstanceComputeMetadata));
-                var malformedJsonStream = new MemoryStream();
-                serializer.WriteObject(malformedJsonStream, malformedData);
+                var malformedJsonStream = this.GetTestMetadataStream(malformedData);
 
-                // Construct a response just like in the positive test but triple it.
                 response.ContentEncoding = Encoding.UTF8;
-
-                // Get a response stream and write the response to it.
                 response.ContentLength64 = (int)malformedJsonStream.Length;
                 context.Response.ContentType = "application/json";
                 malformedJsonStream.WriteTo(context.Response.OutputStream);
-                context.Response.StatusCode = 200;
             }))
             {
                 var azureIms = new AzureMetadataRequestor
                 {
-                    BaseAimsUri = mockUri
+                    BaseAimsUri = AzureInstanceMetadataEndToEndTests.MockTestUri
                 };
 
                 var azureImsProps = new AzureComputeMetadataHeartbeatPropertyProvider(azureIms);
@@ -183,32 +143,26 @@ namespace Microsoft.ApplicationInsights.WindowsServer
         {
             CancellationTokenSource cts = new CancellationTokenSource();
 
-            string mockUri = "http://localhost:9922/";
-
             using (new AzureInstanceMetadataServiceMock(
-                mockUri, 
+                AzureInstanceMetadataEndToEndTests.MockTestUri, 
                 (HttpListenerContext context) =>
             {
                 // wait for longer than the request timeout
                 System.Threading.Thread.Sleep(TimeSpan.FromSeconds(5));
 
                 HttpListenerResponse response = context.Response;
-
-                // Construct a response just like in the positive test but triple it.
-                response.ContentEncoding = Encoding.UTF8;
-
-                // Get a response stream and write the response to it.
-                this.jsonStream.Position = 0;
-                response.ContentLength64 = (int)this.jsonStream.Length;
-                context.Response.ContentType = "application/json";
-                this.jsonStream.WriteTo(context.Response.OutputStream);
-
                 context.Response.StatusCode = 200;
+
+                response.ContentEncoding = Encoding.UTF8;
+                var jsonStream = this.GetTestMetadataStream();
+                response.ContentLength64 = (int)jsonStream.Length;
+                context.Response.ContentType = "application/json";
+                jsonStream.WriteTo(context.Response.OutputStream);
             }))
             {
                 var azureIms = new AzureMetadataRequestor
                 {
-                    BaseAimsUri = mockUri,
+                    BaseAimsUri = AzureInstanceMetadataEndToEndTests.MockTestUri,
                     AzureImsRequestTimeout = TimeSpan.FromSeconds(1)
                 };
 
@@ -224,18 +178,17 @@ namespace Microsoft.ApplicationInsights.WindowsServer
         {
             CancellationTokenSource cts = new CancellationTokenSource();
 
-            string mockUri = "http://localhost:9922/";
-
             using (new AzureInstanceMetadataServiceMock(
-                mockUri, 
+                AzureInstanceMetadataEndToEndTests.MockTestUri, 
                 (HttpListenerContext context) =>
             {
+                // don't send anything in content at all, or the context defaults to 200 OK
                 context.Response.StatusCode = (int)HttpStatusCode.Forbidden;
             }))
             {
                 var azureIms = new AzureMetadataRequestor
                 {
-                    BaseAimsUri = mockUri
+                    BaseAimsUri = AzureInstanceMetadataEndToEndTests.MockTestUri
                 };
 
                 var azureIMSData = azureIms.GetAzureComputeMetadataAsync();
@@ -243,6 +196,52 @@ namespace Microsoft.ApplicationInsights.WindowsServer
 
                 Assert.Null(azureIMSData.Result);
             }
+        }
+
+        /// <summary>
+        /// Return a memory stream adequate for testing.
+        /// </summary>
+        /// <param name="inst">An Azure instance metadata compute object.</param>
+        /// <returns>Azure Instance Compute Metadata as a JSON-encoded MemoryStream.</returns>
+        private MemoryStream GetTestMetadataStream(AzureInstanceComputeMetadata inst = null)
+        {
+            if (inst == null)
+            {
+                inst = this.GetTestMetadata();
+            }
+
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(AzureInstanceComputeMetadata));
+
+            MemoryStream jsonStream = new MemoryStream();
+            serializer.WriteObject(jsonStream, inst);
+
+            return jsonStream;
+        }
+
+        /// <summary>
+        /// Creates test data for heartbeat e2e.
+        /// </summary>
+        /// <returns>An Azure Instance Metadata Compute object suitable for use in testing.</returns>
+        private AzureInstanceComputeMetadata GetTestMetadata()
+        {
+            return new AzureInstanceComputeMetadata()
+            {
+                Location = "US-West",
+                Name = "test-vm01",
+                Offer = "D9_USWest",
+                OsType = "Linux",
+                PlacementGroupId = "placement-grp",
+                PlatformFaultDomain = "0",
+                PlatformUpdateDomain = "0",
+                Publisher = "Microsoft",
+                ResourceGroupName = "test.resource-group_01",
+                Sku = "Windows_10",
+                SubscriptionId = Guid.NewGuid().ToString(),
+                Tags = "thisTag;thatTag",
+                Version = "10.8a",
+                VmId = Guid.NewGuid().ToString(),
+                VmSize = "A8"
+            };
         }
     }
 }
