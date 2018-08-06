61aac47b41550f9ada005462786888f247602871
Liudmila Molkova <lmolkova@microsoft.com>
2018-08-06 16:00:40 -0700

    minor fixes

--- Src/Common/W3C/W3CActivityExtensions.cs
+++ Src/Common/W3C/W3CActivityExtensions.cs
@@ -3,7 +3,6 @@
     using System;
     using System.ComponentModel;
     using System.Diagnostics;
-    using System.Globalization;
     using System.Linq;
     using Microsoft.ApplicationInsights.Common;
 
