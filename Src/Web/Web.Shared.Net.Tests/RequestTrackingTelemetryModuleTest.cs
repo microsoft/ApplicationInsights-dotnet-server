51c76811dd35da3e365c4ed59eeff678a62c75dd
Liudmila Molkova <lmolkova@microsoft.com>
2018-10-18 12:44:51 -0700

    up

--- Src/Web/Web.Shared.Net.Tests/RequestTrackingTelemetryModuleTest.cs
+++ Src/Web/Web.Shared.Net.Tests/RequestTrackingTelemetryModuleTest.cs
@@ -490,10 +490,12 @@
                 config = this.CreateDefaultConfig(HttpModuleHelper.GetFakeHttpContext());
             }
 
+#pragma warning disable 612, 618
             if (enableW3CTracing)
             {
                 config.TelemetryInitializers.Add(new W3COperationCorrelationTelemetryInitializer());
             }
+#pragma warning restore 612, 618
 
             module.Initialize(config);
 
