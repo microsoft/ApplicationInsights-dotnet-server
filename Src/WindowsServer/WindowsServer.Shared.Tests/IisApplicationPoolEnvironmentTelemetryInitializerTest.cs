f54b12cbbd5d618e06dc7c57b296f65787a29f63
paulomorgado <470455+paulomorgado@users.noreply.github.com>
2019-03-10 22:25:50 +0000

    Added IIS application pool telemetry initializer.

---
+++ Src/WindowsServer/WindowsServer.Shared.Tests/IisApplicationPoolEnvironmentTelemetryInitializerTest.cs
@@ -0,0 +1,157 @@
+﻿namespace Microsoft.ApplicationInsights.WindowsServer
+{
+    using System;
+    using System.Globalization;
+    using Microsoft.ApplicationInsights.DataContracts;
+    using Microsoft.ApplicationInsights.Extensibility.Implementation;
+#if !NETCORE
+    using Microsoft.ApplicationInsights.WindowsServer.Azure;
+    using Microsoft.ApplicationInsights.WindowsServer.Azure.Emulation;
+#endif
+    using Microsoft.ApplicationInsights.WindowsServer.Implementation;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Assert = Xunit.Assert;
+
+    [TestClass]
+    public class IisApplicationPoolEnvironmentTelemetryInitializerTest
+    {
+        private const string RoleName = "TestRoleName";
+        private const string ComputerName = "TestComputerName";
+
+        [TestMethod]
+        public void IisApplicationPoolTelemetryInitializerSetsRoleName()
+        {
+            string appPoolTestVarName, computerTestVarName;
+            GetAndInitializeEnvironment(out appPoolTestVarName, out computerTestVarName);
+
+            var telemetryItem = new EventTelemetry();
+
+            var initializer = new IisApplicationPoolTelemetryInitializer(appPoolTestVarName, computerTestVarName);
+
+            initializer.Initialize(telemetryItem);
+
+            Assert.Equal(RoleName, telemetryItem.Context.Cloud.RoleName);
+
+            ClearEnvironmentVariables(appPoolTestVarName, computerTestVarName);
+        }
+
+        [TestMethod]
+        public void IisApplicationPoolTelemetryInitializerSetsRoleInstanceNameAndNodeName()
+        {
+            string appPoolTestVarName, computerTestVarName;
+            GetAndInitializeEnvironment(out appPoolTestVarName, out computerTestVarName);
+
+            var telemetryItem = new EventTelemetry();
+
+            var initializer = new IisApplicationPoolTelemetryInitializer(appPoolTestVarName, computerTestVarName);
+
+            initializer.Initialize(telemetryItem);
+
+            Assert.Equal(ComputerName, telemetryItem.Context.Cloud.RoleInstance);
+            Assert.Equal(ComputerName, telemetryItem.Context.GetInternalContext().NodeName);
+
+            ClearEnvironmentVariables(appPoolTestVarName, computerTestVarName);
+        }
+
+        [TestMethod]
+        public void IisApplicationPoolTelemetryInitializerDoesNotOverrideRoleName()
+        {
+            const string TestValue = "Test";
+            string appPoolTestVarName, computerTestVarName;
+            GetAndInitializeEnvironment(out appPoolTestVarName, out computerTestVarName);
+
+            var telemetryItem = new EventTelemetry();
+            telemetryItem.Context.Cloud.RoleName = TestValue;
+
+            var initializer = new IisApplicationPoolTelemetryInitializer(appPoolTestVarName, computerTestVarName);
+
+            initializer.Initialize(telemetryItem);
+
+            Assert.Equal(TestValue, telemetryItem.Context.Cloud.RoleName);
+            Assert.Equal(ComputerName, telemetryItem.Context.Cloud.RoleInstance);
+            Assert.Equal(ComputerName, telemetryItem.Context.GetInternalContext().NodeName);
+
+            ClearEnvironmentVariables(appPoolTestVarName, computerTestVarName);
+        }
+
+        [TestMethod]
+        public void IisApplicationPoolTelemetryInitializerDoesNotOverrideRoleInstance()
+        {
+            const string TestValue = "Test";
+            string appPoolTestVarName, computerTestVarName;
+            GetAndInitializeEnvironment(out appPoolTestVarName, out computerTestVarName);
+
+            var telemetryItem = new EventTelemetry();
+            telemetryItem.Context.Cloud.RoleInstance = TestValue;
+
+            var initializer = new IisApplicationPoolTelemetryInitializer(appPoolTestVarName, computerTestVarName);
+
+            initializer.Initialize(telemetryItem);
+
+            Assert.Equal(RoleName, telemetryItem.Context.Cloud.RoleName);
+            Assert.Equal(TestValue, telemetryItem.Context.Cloud.RoleInstance);
+            Assert.Equal(ComputerName, telemetryItem.Context.GetInternalContext().NodeName);
+
+            ClearEnvironmentVariables(appPoolTestVarName, computerTestVarName);
+        }
+
+        [TestMethod]
+        public void IisApplicationPoolTelemetryInitializerDoesNotOverrideNodeName()
+        {
+            const string TestValue = "Test";
+            string appPoolTestVarName, computerTestVarName;
+            GetAndInitializeEnvironment(out appPoolTestVarName, out computerTestVarName);
+
+            var telemetryItem = new EventTelemetry();
+            telemetryItem.Context.GetInternalContext().NodeName = TestValue;
+
+            var initializer = new IisApplicationPoolTelemetryInitializer(appPoolTestVarName, computerTestVarName);
+
+            initializer.Initialize(telemetryItem);
+
+            Assert.Equal(RoleName, telemetryItem.Context.Cloud.RoleName);
+            Assert.Equal(ComputerName, telemetryItem.Context.Cloud.RoleInstance);
+            Assert.Equal(TestValue, telemetryItem.Context.GetInternalContext().NodeName);
+
+            ClearEnvironmentVariables(appPoolTestVarName, computerTestVarName);
+        }
+
+        [TestMethod]
+        public void IisApplicationPoolTelemetryInitializerEmptyVariable()
+        {
+            string appPoolTestVarName, computerTestVarName;
+            GetAndInitializeEnvironment(out appPoolTestVarName, out computerTestVarName);
+            Environment.SetEnvironmentVariable(appPoolTestVarName, null);
+            Environment.SetEnvironmentVariable(computerTestVarName, null);
+
+            var telemetryItem = new EventTelemetry();
+
+            var initializer = new IisApplicationPoolTelemetryInitializer(appPoolTestVarName, computerTestVarName);
+
+            initializer.Initialize(telemetryItem);
+
+            Assert.Null(telemetryItem.Context.Cloud.RoleName);
+            Assert.Null(telemetryItem.Context.Cloud.RoleInstance);
+            Assert.Null(telemetryItem.Context.GetInternalContext().NodeName);
+
+            ClearEnvironmentVariables(appPoolTestVarName, computerTestVarName);
+        }
+
+        private static void GetAndInitializeEnvironment(out string appPoolVarName, out string computerVarName)
+        {
+            appPoolVarName = "APP_POOL_ID_" + Guid.NewGuid().ToString();
+            Environment.SetEnvironmentVariable(appPoolVarName, RoleName);
+
+            computerVarName = "COMPUTERNAME_" + Guid.NewGuid().ToString();
+            Environment.SetEnvironmentVariable(computerVarName, ComputerName);
+        }
+
+        private static void ClearEnvironmentVariables(string appPoolVarName, string computerVarName)
+        {
+            Environment.SetEnvironmentVariable(appPoolVarName, null);
+
+            Environment.SetEnvironmentVariable(computerVarName, null);
+        }
+    }
+}
---
+++ Src/WindowsServer/WindowsServer.Shared/IisApplicationPoolTelemetryInitializer.cs
@@ -0,0 +1,92 @@
+﻿namespace Microsoft.ApplicationInsights.WindowsServer
+{
+    using System;
+    using Microsoft.ApplicationInsights.Channel;
+    using Microsoft.ApplicationInsights.Extensibility;
+    using Microsoft.ApplicationInsights.Extensibility.Implementation;
+    using Microsoft.ApplicationInsights.WindowsServer.Implementation;
+
+    /// <summary>
+    /// A telemetry initializer that will gather IIS application pool context information.
+    /// </summary>
+    public sealed class IisApplicationPoolTelemetryInitializer : ITelemetryInitializer, IDisposable
+    {
+        private readonly string iisApplicationPoolNameEnvironmentVariable;
+        private readonly string computerNameEnvironmentVariable;
+        private string roleName;
+        private string roleInstanceName;
+        private volatile bool updateEnvVars = true;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IisApplicationPoolTelemetryInitializer" /> class.
+        /// </summary>
+        public IisApplicationPoolTelemetryInitializer()
+            : this("APP_POOL_ID", "COMPUTERNAME")
+        {
+        }
+
+        internal IisApplicationPoolTelemetryInitializer(string iisApplicationPoolNameEnvironmentVariable, string computerNameEnvironmentVariable)
+        {
+            this.iisApplicationPoolNameEnvironmentVariable = iisApplicationPoolNameEnvironmentVariable;
+            this.computerNameEnvironmentVariable = computerNameEnvironmentVariable;
+            WindowsServerEventSource.Log.TelemetryInitializerLoaded(this.GetType().FullName);
+            AppServiceEnvironmentVariableMonitor.Instance.MonitoredAppServiceEnvVarUpdatedEvent += this.UpdateEnvironmentValues;
+        }
+
+        /// <summary>
+        /// Initializes <see cref="ITelemetry" /> role and node context information.
+        /// </summary>
+        /// <param name="telemetry">The telemetry to initialize.</param>
+        public void Initialize(ITelemetry telemetry)
+        {
+            if (this.updateEnvVars)
+            {
+                this.roleName = this.GetRoleName();
+                this.roleInstanceName = this.GetRoleInstanceName();
+                this.updateEnvVars = false;
+            }
+
+            if (string.IsNullOrEmpty(telemetry.Context.Cloud.RoleName))
+            {
+                telemetry.Context.Cloud.RoleName = this.roleName;
+            }
+
+            if (string.IsNullOrEmpty(telemetry.Context.Cloud.RoleInstance))
+            {
+                telemetry.Context.Cloud.RoleInstance = this.roleInstanceName;
+            }
+
+            if (string.IsNullOrEmpty(telemetry.Context.GetInternalContext().NodeName))
+            {
+                telemetry.Context.GetInternalContext().NodeName = this.roleInstanceName;
+            }
+        }
+
+        /// <summary>
+        /// Remove our event handler from the environment variable monitor.
+        /// </summary>
+        public void Dispose()
+        {
+            AppServiceEnvironmentVariableMonitor.Instance.MonitoredAppServiceEnvVarUpdatedEvent -= this.UpdateEnvironmentValues;
+        }
+
+        private string GetRoleName()
+        {
+            string roleName = string.Empty;
+            AppServiceEnvironmentVariableMonitor.Instance.GetCurrentEnvironmentVariableValue(this.iisApplicationPoolNameEnvironmentVariable, ref roleName);
+            return roleName ?? string.Empty;
+        }
+
+        private string GetRoleInstanceName()
+        {
+            string roleInstanceName = string.Empty;
+            AppServiceEnvironmentVariableMonitor.Instance.GetCurrentEnvironmentVariableValue(this.computerNameEnvironmentVariable, ref roleInstanceName);
+            return roleInstanceName ?? string.Empty;
+        }
+
+        private void UpdateEnvironmentValues()
+        {
+            this.updateEnvVars = true;
+        }
+    }
+}
