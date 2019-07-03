f954e9b953c62510e6e283dea5e08df17dbe4d15
Cijo Thomas <cithomas@microsoft.com>
2019-07-03 09:39:55 -0700

    add report as

--- Src/EventCounterCollector/EventCounterCollector/EventCounterCollectionRequest.cs
+++ Src/EventCounterCollector/EventCounterCollector/EventCounterCollectionRequest.cs
@@ -17,10 +17,21 @@
         /// </summary>
         /// <param name="eventSourceName">EventSourceName which publishes the counter.</param>
         /// <param name="eventCounterName">name of the counter.</param>
-        public EventCounterCollectionRequest(string eventSourceName, string eventCounterName)
+        public EventCounterCollectionRequest(string eventSourceName, string eventCounterName) : this(eventSourceName, eventCounterName, string.Empty)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EventCounterCollectionRequest"/> class.
+        /// </summary>
+        /// <param name="eventSourceName">EventSourceName which publishes the counter.</param>
+        /// <param name="eventCounterName">name of the counter.</param>
+        /// <param name="reportAs">override reporting name of the counter.</param>
+        public EventCounterCollectionRequest(string eventSourceName, string eventCounterName, string reportAs)
         {
             this.EventSourceName = eventSourceName;
             this.EventCounterName = eventCounterName;
+            this.ReportAs = reportAs;
         }
 
         /// <summary>
@@ -32,5 +43,10 @@
         /// Gets or sets the name of the counter.
         /// </summary>
         public string EventCounterName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the counter to be used as Metric name. If none specified, the DisplayName of the EventCounter will be used.
+        /// </summary>
+        public string ReportAs { get; set; }
     }
 }
